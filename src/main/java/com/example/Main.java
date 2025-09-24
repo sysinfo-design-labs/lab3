83e2c0f6147c66c6407137a09dcd1634729031a5
nyash <savell99@mail.ru>
2025-09-24 11:59:01 +0500

    Add class to solve first task

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,12 +1,21 @@
 package com.example;
 
+import app.FirstTaskMetrics;
+
 public class Main {
-
-    public static String helloWorld() {
-        return "Hello, World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(helloWorld());
+
+        double languageLevel = 1.6;
+        int[] data = {25, 28, 8, 3};
+
+        FirstTaskMetrics task1 = new FirstTaskMetrics(
+                data[0],
+                data[1],
+                data[2],
+                data[3],
+                languageLevel
+        );
+
+        task1.output();
     }
 }
